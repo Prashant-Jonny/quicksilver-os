0cd2b68b1e2294d0062c84a6c3d57513b5affb7f
John <quicksilverproducts@outlook.com>
2013-01-30 20:17:59 -0500

    Praxis username stuff

--- Quicksilver2013/Kernel.cs
+++ Quicksilver2013/Kernel.cs
@@ -31,11 +31,13 @@ namespace Quicksilver2013
             }
             #endregion
             #region GLNFS
+            /*
             GruntyOS.HAL.ATA.Detect(); // This will detect all ATA devices and add them to the device filesystem
             GruntyOS.CurrentUser.Privilages = 0; // This has to be set, 1 = limited 0 = root
             GruntyOS.CurrentUser.Username = "Admin"; // When using anything in the class File this will be the default username
 
             GruntyOS.HAL.FileSystem.Root = new GruntyOS.HAL.RootFilesystem(); // initialize virtual filesystem
+            bool ispart = false;
             for (int i = 0; i < GruntyOS.HAL.Devices.dev.Count; i++)
             {
                 if (GruntyOS.HAL.Devices.dev[i].dev is Cosmos.Hardware.BlockDevice.Partition)
@@ -49,18 +51,21 @@ namespace Quicksilver2013
                     }
                     else { Console.Write("Filesystem Label: "); new GruntyOS.HAL.GLNFS((Cosmos.Hardware.BlockDevice.Partition)GruntyOS.HAL.Devices.dev[i].dev).Format(Console.ReadLine()); }
                     GruntyOS.HAL.FileSystem.Root.Mount("/" + FS.DriveLabel, FS); // mount it as root (you can only have on partition mounted as root!!!!
+                    if (!ispart) new fdisk().Execute(new string[1]);
                 }
             }
-
+            */
             #endregion
             Parser.Init();
             mouse.Initialize();
-            Console.WriteLine("Quicksilver OS Alpha 1.0.0.18 as of 130129-1810\r\nCopyright (c) 2013");
-            mouse.Initialize();
+            Console.WriteLine("Quicksilver OS Alpha 1.0.0.19 as of 130130-2010\r\nCopyright (c) 2013");
+            Console.Write("Welcome to Quicksilver OS. Please Pick a username: ");
+            UserService.user = Console.ReadLine();
+            Console.Clear();
         }
         protected override void Run()
         {
-            QuicksilverNEXT.Console.Write(GruntyOS.CurrentUser.Username + "@" + cd + "# ");
+            QuicksilverNEXT.Console.Write(UserService.user + "@" + cd + "# ");
             QuicksilverNEXT.Console.Flush();
             commandwpar = Console.ReadLine();
             Parser.Parse(commandwpar);
