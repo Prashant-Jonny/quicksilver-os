d8f4fafac1338a67c3399f8ff2c67fed0b492844
John <quicksilverproducts@outlook.com>
2013-02-11 20:40:07 -0500

    FileSystem works
    Also, exits (I dont know why)

--- Quicksilver2013/Kernel.cs
+++ Quicksilver2013/Kernel.cs
@@ -25,26 +25,19 @@ namespace Quicksilver2013
         Cosmos.Hardware.Mouse mouse = new Cosmos.Hardware.Mouse();
         protected override void BeforeRun()
         {
-            #region SMBIOS
-            SMBIOS.SMBIOS smbios = new SMBIOS.SMBIOS();
-            //gets smbios
-            if (smbios.CheckSMBIOS())
-            {
-                smbios.GetSMBIOS_Data();
-                List<SMBIOS.BaseInfo> bi = smbios.GetHardwareDescriptorByType( SMBIOS.TableTypes.ProcessorInformation );
-                if(bi.Count >= 1) {
-                    Console.ReadLine();
-                    cpuid.pi = (SMBIOS.Table.ProcessorInformation)bi[0];
-                }
-            }
-            #endregion
             #region GLNFS
-            
+            Console.ReadLine();
+            vfs = new GDOS.VirtualFileSystem();
+            Console.ReadLine();
             GruntyOS.HAL.ATA.Detect(); // This will detect all ATA devices and add them to the device filesystem
+            Console.ReadLine();
             GruntyOS.CurrentUser.Privilages = 0; // This has to be set, 1 = limited 0 = root
             GruntyOS.CurrentUser.Username = "Admin"; // When using anything in the class File this will be the default username
 
+
             GruntyOS.HAL.FileSystem.Root = new GruntyOS.HAL.RootFilesystem(); // initialize virtual filesystem
+            GruntyOS.HAL.FileSystem.Root.Seperator = '/';
+            Console.ReadLine();
             bool ispart = false;
             for (int i = 0; i < GruntyOS.HAL.Devices.dev.Count; i++)
             {
@@ -54,29 +47,31 @@ namespace Quicksilver2013
 
                     if (GruntyOS.HAL.GLNFS.isGFS((Cosmos.Hardware.BlockDevice.Partition)GruntyOS.HAL.Devices.dev[i].dev))
                     {
-                        Console.WriteLine("Drive detected!");
+                        Console.WriteLine("Drive Detected");
+                        Console.ReadLine();
                         ispart = true;
-                    }
-                    else { Console.Write("Filesystem Label: "); new GruntyOS.HAL.GLNFS((Cosmos.Hardware.BlockDevice.Partition)GruntyOS.HAL.Devices.dev[i].dev).Format(Console.ReadLine()); }
-                    if (GruntyOS.HAL.GLNFS.isGFS((Cosmos.Hardware.BlockDevice.Partition)GruntyOS.HAL.Devices.dev[i].dev) && !ispart)
-                    {
-                        GruntyOS.HAL.FileSystem.Root.Mount("/" , FS); // mount it as root (you can only have on partition mounted as root!!!!
- 
                         cd = "/";
                         var d = new GDOS.Drive();
                         d.Filesystem = FS;
                         d.DeviceFile = GruntyOS.HAL.Devices.dev[i].name;
                         vfs.AddDrive(d);
-                        ispart = true;
+                        GruntyOS.HAL.FileSystem.Root.Mount("/", FS);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.Write("Filesystem Label: "); new GruntyOS.HAL.GLNFS((Cosmos.Hardware.BlockDevice.Partition)GruntyOS.HAL.Devices.dev[i].dev).Format(Console.ReadLine());
+                        //Cosmos.Core.Global.CPU.Reboot();
                     }
                 }
             }
             if (!ispart) new fdisk().Execute(new string[1]);
             #endregion
-            vfs.saveFile(Quicksilver2013.Files.Exes.Example_exe, "/example.exe", "Admin");
-            Console.WriteLine("Welcome to Quicksilver OS Alpha 1.0.0.30 as of 130210\r\nCopyright (c) 2013");
+            vfs.makeDir("/root", "Admin");
+            vfs.saveFile(Quicksilver2013.Files.Exes.Example_exe, "/root/example.exe", "Admin");
+            //Console.ReadLine();
+            Console.WriteLine("Welcome to Quicksilver OS Alpha 1.0.0.31 as of 130211\r\nCopyright (c) 2013");
             Console.Write("Please pick a username: ");
-            new Quicksilver2013.Executable.PE32("/example.exe");
             UserService.user = Console.ReadLine();
             Parser.Init();
             Console.Clear();
